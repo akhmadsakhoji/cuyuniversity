34e2e15115f73a83657d42c33577e5657c5c30ad
Akhmad Sakhoji Jamaludin <akhmadsakhoji@gmail.com>
2022-08-16 08:18:07 +0700

    selesai course semester 2 sesi-1

--- semester-2/sesi-1/src/App.js
+++ semester-2/sesi-1/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Introduction from './components/introduction';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile />
+      <Introduction name="Akhmad Sakhoji Jamaludin" />
+      <Introduction name="Diah Ayu Putri Caturningtyas"/>
     </div>
   );
 }
